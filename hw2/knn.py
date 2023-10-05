74755f35b436fc276bdea8dfd8658c9819584c02
Jason Jones <jasojone@gmail.com>
2023-10-05 14:41:12 -0700

    completed

--- hw2/knn.py
+++ hw2/knn.py
@@ -20,23 +20,26 @@ with open('binary_points.csv', 'r') as csvfile:
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
          db.append (row)
-
+         
+errors = 0
 #loop your data to allow each instance to be your test set
-for ? in db:
+for i, instance in enumerate(db):
 
-    #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to
-    # float to avoid warning messages
-    #--> add your Python code here
-    # X =
-
-    #transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. For instance, Y = [1, 2, ,...]. Convert each
-    #  feature value to float to avoid warning messages
-    #--> add your Python code here
-    # Y =
+    #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. 
+    # For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to float to avoid warning messages
+    X = []
+    
+    #transform the original training classes to numbers and add to the vector Y removing the instance that will be used 
+    # for testing in this iteration. For instance, Y = [1, 2, ,...]. Convert each feature value to float to avoid warning messages
+    Y = []
 
     #store the test sample of this iteration in the vector testSample
-    #--> add your Python code here
-    #testSample =
+    testSample = [float(instance[0]), float(instance[1])]
+    testLabel = instance[2]
+    for j, row in enumerate(db):
+        if i != j:
+            X.append([float(row[0]), float(row[1])])
+            Y.append(row[2])
 
     #fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
@@ -44,14 +47,14 @@ for ? in db:
 
     #use your test sample in this iteration to make the class prediction. For instance:
     #class_predicted = clf.predict([[1, 2]])[0]
-    #--> add your Python code here
-
+    class_predicted = clf.predict([testSample])[0]
     #compare the prediction with the true label of the test instance to start calculating the error rate.
-    #--> add your Python code here
-
+    if class_predicted != testLabel:
+        errors += 1
 #print the error rate
 #--> add your Python code here
-
+error_rate = errors / len(db)
+print("Error rate for 1NN: ", error_rate)
 
 
 
--- hw2/naive_bayes.py
+++ hw2/naive_bayes.py
@@ -1,7 +1,8 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Jason Jones
+# FILENAME: naive_bayes.py
+# SPECIFICATION: 
+# This program reads in a training set and a test set and trains a naive bayes classifier on the training set.
 # FOR: CS 4210- Assignment #2
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
@@ -14,16 +15,30 @@ from sklearn.naive_bayes import GaussianNB
 
 #reading the training data in a csv file
 #--> add your Python code here
+import csv
+training_data = []
+with open('weather_training.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        if i > 0: #skipping the header
+            training_data.append(row)
 
 #transform the original training features to numbers and add them to the 4D array X.
 #For instance Sunny = 1, Overcast = 2, Rain = 3, so X = [[3, 1, 1, 2], [1, 3, 2, 2], ...]]
-#--> add your Python code here
-# X =
+outlook_feature = {"Sunny": 1, "Overcast": 2, "Rain": 3}
+temperature_feature = {"Hot": 1, "Mild": 2, "Cool": 3}
+humidity_feature = {"High": 1, "Normal": 2}
+wind_feature = {"Weak": 1, "Strong": 2}
+X = []
+
+for row in training_data:
+    X.append([outlook_feature[row[1]], temperature_feature[row[2]], humidity_feature[row[3]], wind_feature[row[4]]])
 
 #transform the original training classes to numbers and add them to the vector Y.
 #For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
-#--> add your Python code here
-# Y =
+class_mapping = {"Yes": 1, "No": 2}
+Y = [class_mapping[row[5]] for row in training_data]
+# print(Y)
 
 #fitting the naive bayes to the data
 clf = GaussianNB()
@@ -31,11 +46,25 @@ clf.fit(X, Y)
 
 #reading the test data in a csv file
 #--> add your Python code here
+test_data = []
+with open('weather_test.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        if i > 0: #skipping the header
+            test_data.append(row)
 
 #printing the header os the solution
-#--> add your Python code here
+print("The predictions with probabilities greater than 0.75 of weather_test.csv using the naive bayes classifier are:")
 
 #use your test samples to make probabilistic predictions. For instance: clf.predict_proba([[3, 1, 2, 1]])[0]
-#--> add your Python code here
+print("Sample    Outlook    Temperature    Humidity    Windy    Play    Probability")
+for row in test_data:
+    test_sample = [outlook_feature[row[1]], temperature_feature[row[2]], humidity_feature[row[3]], wind_feature[row[4]]]
+    probability = clf.predict_proba([test_sample])[0]
+    if probability[0] >= 0.75:
+        print(f"{row[0]:<10}{row[1]:<12}{row[2]:<15}{row[3]:<12}{row[4]:<10}Yes      {probability[0]:.2f}")
+    elif probability[1] >= 0.75:
+        print(f"{row[0]:<10}{row[1]:<12}{row[2]:<15}{row[3]:<12}{row[4]:<10}No       {probability[1]:.2f}")
+
 
 
